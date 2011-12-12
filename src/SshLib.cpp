4d663efd1b594c2a9a0cd8835ca696c0eb893462
Jereak <jereakk@gmail.com>
2011-12-12 21:30:46 +0100

    salut

---
+++ src/SshLib.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : SshLib.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello gropabo!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
